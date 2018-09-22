534272629d54beb855f9dc284747484d6fdcc591
Anri <amedamachip713@gmail.com>
2018-09-22 12:12:14 -0500

    Add word class

---
+++ tests/hello/hello_spec.rb
@@ -0,0 +1,11 @@
+require_relative 'hello'
+
+RSpec.describe Hello do
+
+  it "should return 'Hello World!' " do
+    greeting = Hello.say_hello
+    #expect the output "Hello World!"
+    expect(greeting).to eq("Hello World!")
+  end
+
+end
