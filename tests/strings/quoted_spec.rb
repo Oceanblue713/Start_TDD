ffd3aee599523b6d62efbbe826f251e4711f4122
Anri <amedamachip713@gmail.com>
2018-10-05 17:27:02 -0500

    Add strings spec

---
+++ tests/strings/quoted_spec.rb
@@ -0,0 +1,32 @@
+require_relative 'quoted'
+
+RSpec.describe QuotedString do
+  before do
+    @placeholder = 5 + 10
+  end
+
+  context "quoted with %q" do
+    it "should not out put interpolated text" do
+      sentence = %q{The sum of 5 + 10 is: #{@placeholder}}
+      interpolated = "The sum of 5 + 10 is: 15"
+
+      text = QuotedString.new sentence
+
+      expect(text.to_s.start_with?('The sum')).to eq(true)
+      expect(text).not_to eq(interpolated)
+    end
+  end
+
+  context "quoted with %Q" do
+    it "should out put interpolated text" do
+      sentence = %Q{The sum of 5 + 10 is: #{@placeholder}}
+      interpolated = "The sum of 5 + 10 is: 15"
+
+      text = QuotedString.new sentence
+
+      expect(text.to_s).to eq(true)
+
+      expect(text).not_to eq(interpolated)
+    end
+  end
+end
