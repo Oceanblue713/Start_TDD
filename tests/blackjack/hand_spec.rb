17b7f1a94132df8e9e67ac798940ce791853ecc4
Anri Liy <anri@realquantum.com>
2018-11-18 09:41:39 -0600

    Add hand_spec.rb tests

--- tests/blackjack/hand_spec.rb
+++ tests/blackjack/hand_spec.rb
@@ -16,25 +16,64 @@ RSpec.describe Hand do
     end
 
     it "returns the correct number of cards added" do 
+      card1 = Card.new("Diamonds", "Jack")
+      card2 = Card.new("Clubs", "Ace")
+
+      @hand.add_card card1
+      @hand.add_card card2
+
+      expect(@hand.dealt_cards.size).to eq(2)
+
+      expect(@hand.dealt_cards.first.suit).to eq("Diamonds")
+      expect(@hand.dealt_cards.first.rank).to eq("Jack")
+      expect(@hand.dealt_cards.last.suit).to eq("Clubs")
+      expect(@hand.dealt_cards.last.rank).to eq("Ace")
     end
   end
 
   describe "#get_value" do 
     it "responds to get_value" do 
+      expect(@hand).to respond_to(:get_value)
     end
 
     it "returns correct value with no Ace" do 
+      card1 = Card.new("Diamonds", "Jack")
+      card2 = Card.new("Clubs", "9")
+
+      @hand.add_card card1 
+      @hand.add_card card2
+
+      expect(@hand.get_value).to eq 19
     end
 
     it "returns correct value with an Ace and a Jack" do 
+      card1 = Card.new("Diamonds", "Jack")
+      card2 = Card.new("Clubs", "Ace")
+
+      @hand.add_card card1 
+      @hand.add_card card2
+
+      expect(@hand.get_value).to eq 21
     end
   end
 
   describe "Hand output" do
-    it "returns the correct output if 'show' is true for all cards" do 
+    it "returns the correct output if 'show' is true for all cards" do
+      @hand.add_card Card.new("Diamonds", "Jack")
+      @hand.add_card Card.new("Clubs", "Ace")
+
+      expect("#{@hand}").to eq("Jack of Diamonds, Ace of Clubs, Total value: 21")
     end
 
-    it "returns the correct output if 'show' is false for one card" do 
+    it "returns the correct output if 'show' is false for one card" do
+      card1 = Card.new("Diamonds", "Jack")
+      card2 = Card.new("Clubs", "Ace")
+      card1.show = false
+
+      @hand.add_card card1 
+      @hand.add_card card2
+
+      expect("#{@hand}").to eq ("Ace of Clubs, Total value: 11")
     end
   end
 
